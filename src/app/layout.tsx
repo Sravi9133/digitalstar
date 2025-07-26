3428b149f09e341315ebe24b431f5092ae309963
no reply <noreply.redditmonitoring@gmail.com>
2025-07-26 10:56:08 +0000

    Collecting page data ...    Generating static pages (0/8) ...  ⨯ useSear

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { RefTracker } from '@/components/ref-tracker';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
   title: 'Digital Star LPU',
@@ -21,7 +22,9 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased min-h-screen flex flex-col">
-        <RefTracker />
+        <Suspense fallback={null}>
+          <RefTracker />
+        </Suspense>
         {children}
         <Toaster />
       </body>
